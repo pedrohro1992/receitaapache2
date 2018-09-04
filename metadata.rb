a675bf2a275610139f9e8b1487e2c3361aa4c641
pedrohro1992 <pedro_hro1992@outlook.com>
2018-09-04 15:46:37 +0000

    Adicionando os novos arquivos

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'receitaapache2'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures receitaapache2'
+long_description 'Installs/Configures receitaapache2'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/receitaapache2/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/receitaapache2' if respond_to?(:source_url)
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: receitaapache2
+# Recipe:: default
+#
+# Copyright (c) 2018 The Authors, All Rights Reserved.
