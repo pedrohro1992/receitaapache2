9710e1367ecb68de470eecb689a50c372fdea489
root <root@centos7-chef.c.chef-aulas.internal>
2018-09-11 19:17:20 +0000

    Add node informations to the index page

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,7 +1,11 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do 
-	content '<h1>Hello, Word!</h1>'
+	content "<h1> Informações sobre o node</h1>
+	 <h1>HHOSTNAME:#{node['hostname']}</h1>
+	 <h1> IPADDRES: #{node['ipaddress']}</h1> 
+         <h1>CPU: #{node['cpu']['0']['mhz']}</h1>
+         <h1>  MEMORY: #{node['memory']['total']}</h1>"
 end 
 
 service 'httpd' do 
